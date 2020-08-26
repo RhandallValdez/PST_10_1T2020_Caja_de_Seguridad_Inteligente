a007bc41b9a5d7005eb6d7dfa235691a64d8cddb
Rhandall <rhakvald@espol.edu.ec>
2020-08-26 01:44:53 -0500

    Inicio de diseño activity registro caja

--- app/src/main/java/com/example/thebox/registroCaja.java
+++ app/src/main/java/com/example/thebox/registroCaja.java
@@ -2,13 +2,33 @@ package com.example.thebox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class registroCaja extends AppCompatActivity {
-
+    private View botonRegistro;
+    private EditText pais,ciudad,calle,detalle,contraseña;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_caja);
+        botonRegistro = findViewById(R.id.mandarRegistroCaja);
+        pais = findViewById(R.id.paisCaja);
+        ciudad = findViewById(R.id.ciudadCaja);
+        calle = findViewById(R.id.calleCaja);
+        detalle=findViewById(R.id.detalleCaja);
+        contraseña = findViewById(R.id.contrasenaCaja);
+        GradientDrawable drawableIngresar = (GradientDrawable) botonRegistro.getBackground();
+        drawableIngresar.setColor(Color.parseColor("#B388FF"));
+    }
+    public void registrar(View v){
+
+    }
+
+    public void regresar(View v){
+        finish();
     }
 }
